03b7162a9dda461339b892f7a57b2ec35e99c0ea
Lea23VC <leandro.villalobos.dx@gmail.com>
2019-12-10 18:26:03 -0300

    Updated syntax and other awful code things

--- bot.js
+++ bot.js
@@ -8,16 +8,8 @@ const auth = require('./auth.json');
 const sagiri = require('sagiri');
 
 
+
 //require('http').createServer().listen(3000);
-
-var params = {
-	'offset': 0,
-	'limit': 10,
-	'sort': 'score',
-	'order': 'desc'
-  };
-
-
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -39,101 +31,33 @@ client.on('message', msg => {
 
  if (msg.content.startsWith('!silence') && !msg.author.tag.endsWith("#3954")) {
 	//var imgflipper = new Imgflipper("Lea23vc", "leandro23");
-	console.log("AWoooo64");
-	let prefix = '!silence';
-	const args = msg.content.slice(prefix.length-1).trim().split(/ +/g);
-
-	args.shift().toLocaleLowerCase();
-
-	console.log(args);
-
-	let text = args.join(" ");
-	let op = text.split(' - ');
-
-	console.log(args);
-
-	var arriba = op[0];
-	var abajo = op[1];
-	console.log(abajo + " " + arriba );
 	
-	var Imgflipper = require('imgflipper');
-	var imgflipper = new Imgflipper("Lea23vc", "leandro23");
-
-	imgflipper.generateMeme(212777853, arriba.toLocaleLowerCase(), abajo, (err, img) => {
-
-		if(err) return console.log(err);
-
-		msg.channel.send(img);
-
-	});
-
-	//getURL(msg);
+	sendmeme(msg);
+	
   }
 
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-	msg.reply('Awoooooo');
-	console.log('awa');
-	
-	//getURL(msg);
-  }
 
 	if (msg.content === '!halp') {
 
-		let embed = new Discord.RichEmbed()
-
-		.setColor('#0099ff')
-		.setTitle('Rom searching commands')
-		//.setURL('https://discord.js.org/')
-		.setAuthor('AigisBot', 'https://i.imgur.com/dbyNfIs.png')
-		.setDescription('piracy=bad, unless you are poor')
-		.setThumbnail('https://i.imgur.com/dbyNfIs.png')
-		//.addBlankField()
-		.addField('Nintendo Entertainment System', '`!nes` or `!famicom`')
-		.addField('Super Nintendo', '`!snes` or `!sfc`')
-		.addField('Nintendo 64', '`!64`')
-		.addField('Gamecube', '`!gamecube` or `!gc`')
-		.addField('Gameboy, Gameboy Color and Gameboy Advance', '`!gb`, `!gbc` and `!gba`')
-		.addField('Nintendo DS', '`!ds` or `!nds`')
-		.addField('PlayStation', '`!psx`')
-
-		//.addField('Touhou main games (not available)', '`!touhou` or `!tojas`')
-		
-		//.addField('Inline field title', 'Some value here', true)
-		//.addField('Inline field title', 'Some value here', true)
-		//.addField('Inline field title', 'Some value here', true)
-		.setImage('https://i.imgur.com/Irk8vtj.png')
-		.setTimestamp()
-		//.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png')
-
-		msg.channel.send(embed)
+		msg.channel.send(halp());
   }
-
-  //if (msg.content.startsWith('!link')) {
-	  //var res = msg.content.slice(5);
-	  //getURL(msg, res);
-  //}
 
 	  
 	if (msg.content === '!myavatar') {
-		// Remove the "var" line; it isn't necessary.
-		let embed = new Discord.RichEmbed()
-		// Replace "message.member" with "message.author"
-		.setImage(msg.author.avatarURL)
-		.setColor('#275BF0')
-		msg.channel.send(embed)
+
+		embedIMG(msg.author.avatarURL, msg);
 	}
 
 
 	if (msg.content === '!sauce') {
 
-		salsa(msg);
+		msg.channel.send(salsa(msg));
 
 	}
 
-	if (msg.content === '!id') {
-		console.log(msg.author.tag);
-	}
+	//if (msg.content === '!id') {
+		//console.log(msg.author.tag);
+	//}
 
 
 	if (msg.content.startsWith('!avatar')) {
@@ -141,13 +65,8 @@ client.on('message', msg => {
 		// Remove the "var" line; it isn't necessary.
 		
 		// Replace "message.member" with "message.author"
-		let embed
 		const avatarList = msg.mentions.users.map(user => { //convierte el mapa de usuarios en una lista, lo cual recorre con la variable "user"
-			embed = new Discord.RichEmbed()
-			.setImage(user.avatarURL)
-			//user.
-			.setColor('#275BF0')
-			msg.channel.send(embed)
+			embedIMG(user.avatarURL, msg);
 		});
 		//msg.channel.send(avatarList);
 		
@@ -155,14 +74,12 @@ client.on('message', msg => {
 	}
 
 	if (msg.content === '!desfunar') {
-		// Remove the "var" line; it isn't necessary.
-		let embed = new Discord.RichEmbed()
-		// Replace "message.member" with "message.author"
-		.setImage("https://i.imgur.com/dr9IspB.png")
-		.setColor('#275BF0')
-		msg.channel.send(embed)
+		
 	}
 
+	//if (msg.content === '!dolar') {
+		//console.log(fx(1).from("USD").to("CLP"));
+	//}
 
 
 	
@@ -247,6 +164,62 @@ client.on('message', msg => {
   
 });
 
+client.login(auth.token);
+//client.login(process.env.token);
+
+
+async function salsa(msg) {
+	msg.channel.fetchMessages({ limit: 25 }).then(messages => {
+
+		found = new Boolean(); //iniciar boolean
+		console.log("AAAGRIA -1");
+
+		messages.forEach(mensaje => {
+			console.log("AAAGRIA -2");
+			if (mensaje.attachments && found) {
+				var lastme = mensaje.attachments.forEach(img => {
+					console.log("AAAAGRIA");
+					if (img.url.endsWith(".png") || img.url.endsWith(".jpg") || img.url.endsWith(".jpeg") || img.url.endsWith(".PNG") || img.url.endsWith(".JPG") ||  img.url.endsWith(".JPEG") ||  img.url.endsWith(".gif") || img.url.endsWith(".GIF")) {
+						//msg.channel.send(img.url);
+						found = false;
+						//console.log("AAAAGRIA3" +  + "Awooooo");
+						saucenao(img.url, msg);
+
+					}
+					
+				});
+			}
+
+
+		})
+
+	})
+
+}
+
+
+
+async function saucenao(img, msg) {
+	
+	var awa = Boolean(true);
+
+	const cliente = sagiri('3397773ef599feb5954db044e6e89e377434dd4b' ,  {results: 10 });
+	console.log("WRYYYY");
+	const salsaa = await cliente(img , { results: 10 });
+	console.log(salsaa);
+	i=0;
+	salsaa.forEach(urls => {
+		console.log(urls.url);
+		if (awa) {
+			msg.channel.send(urls.url);
+			awa=false;
+			console.log("Probando 1 2 3 " + urls.url);
+			//msg.channel.send(urls.thumbnail);
+	
+			
+		}
+	})
+}
 
 
 function getURL(msg, names, folderId){
@@ -302,68 +275,108 @@ function getURL(msg, names, folderId){
   } else {
     console.log('No files found');
 	msg.reply("No files found");
-  }
-});
-
-	
-	
+  	}
+	});
 }	
 
+function halp() {
 
-async function salsa(msg) {
-	msg.channel.fetchMessages({ limit: 25 }).then(messages => {
+	let embed = new Discord.RichEmbed()
 
-		found = new Boolean(); //iniciar boolean
-		console.log("AAAGRIA -1");
+	.setColor('#0099ff')
+	.setTitle('Rom searching commands')
+	//.setURL('https://discord.js.org/')
+	.setAuthor('AigisBot', 'https://i.imgur.com/dbyNfIs.png')
+	.setDescription('piracy=bad, unless you are poor')
+	.setThumbnail('https://i.imgur.com/dbyNfIs.png')
+	//.addBlankField()
+	.addField('Nintendo Entertainment System', '`!nes` or `!famicom`')
+	.addField('Super Nintendo', '`!snes` or `!sfc`')
+	.addField('Nintendo 64', '`!64`')
+	.addField('Gamecube', '`!gamecube` or `!gc`')
+	.addField('Gameboy, Gameboy Color and Gameboy Advance', '`!gb`, `!gbc` and `!gba`')
+	.addField('Nintendo DS', '`!ds` or `!nds`')
+	.addField('PlayStation', '`!psx`')
 
-		messages.forEach(mensaje => {
-			console.log("AAAGRIA -2");
-			if (mensaje.attachments && found) {
-				var lastme = mensaje.attachments.forEach(img => {
-					console.log("AAAAGRIA");
-					if (img.url.endsWith(".png") || img.url.endsWith(".jpg") || img.url.endsWith(".jpeg") || img.url.endsWith(".PNG") || img.url.endsWith(".JPG") ||  img.url.endsWith(".JPEG") ||  img.url.endsWith(".gif") || img.url.endsWith(".GIF")) {
-						//msg.channel.send(img.url);
-						found = false;
-						console.log("AAAAGRIA3");
-						salsa2(img.url, msg);
-
-					}
-					
-				});
-			}
-
-
-		})
-
-	})
-}
-
-async function salsa2(img, msg) {
+	//.addField('Touhou main games (not available)', '`!touhou` or `!tojas`')
 	
-	var awa = Boolean(true);
+	//.addField('Inline field title', 'Some value here', true)
+	//.addField('Inline field title', 'Some value here', true)
+	//.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/Irk8vtj.png')
+	.setTimestamp()
+	//.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png')
 
-	const cliente = sagiri('3397773ef599feb5954db044e6e89e377434dd4b' ,  {results: 10 });
-	console.log("WRYYYY");
-	const salsaa = await cliente(img , { results: 10 });
-	console.log(salsaa);
-	i=0;
-	salsaa.forEach(urls => {
-		console.log(urls.url);
-		if (awa) {
-			msg.channel.send(urls.url);
-			//msg.channel.send(urls.thumbnail);
-			awa=false;
-			
 
-		}
-		
-		
-	})
+	return embed;
 
-		
+
 }
 
-client.login(auth.token);
-//client.login(process.env.token);
+
+function splitmsg(args) {
 
 
+	console.log("AWoooo64");
+	args.shift().toLocaleLowerCase();
+
+	console.log(args);
+
+	let text = args.join(" ");
+	let op = text.split(' - ');
+
+	console.log(op);
+
+	return op;
+
+
+}
+
+async function meme(abajo, arriba, msg) {
+
+	var Imgflipper = require('imgflipper');
+	var imgflipper = new Imgflipper("Lea23vc", "leandro23");
+	console.log("probando meme" + abajo + " " + arriba);
+
+	imgflipper.generateMeme(212777853, abajo, arriba, (err, img) => {
+
+		if(err) return console.log(err);
+
+
+		console.log(img);
+
+		embedIMG(img, msg);
+
+	});
+
+}
+
+
+
+async function sendmeme (msg) {
+
+
+	let prefix = '!silence';
+	const args = msg.content.slice(prefix.length-1).trim().split(/ +/g);
+
+	var op = splitmsg(args);
+	
+	//url = await getURLmeme(op[0], op[1]);
+	//console.log("Probando url: " + url);
+
+	meme(op[0],op[1], msg);
+	
+
+}
+
+async function embedIMG (url, msg) {
+
+	let embed = new Discord.RichEmbed()
+	.setImage(url)
+	//user.
+	.setColor('#275BF0')
+
+	msg.channel.send(embed);
+
+
+}
